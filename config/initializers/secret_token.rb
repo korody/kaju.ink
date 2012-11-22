971b0b67692a043a0cd932da22fa747589506ab1
korody <marcoskorody@hotmail.com>
2012-11-22 16:31:39 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KajuInk::Application.config.secret_token = '73d94abe95cabd47ee289b4d6b263d5c3c1306781896ddde028366195e28f8e44667bde9ea0704ffa4bf812282123c0598237f4941be5eefd56cceabb4b8dc86'
